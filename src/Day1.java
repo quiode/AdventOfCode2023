59c64369c96030f0af3afbe9a46df70b13596943
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2023-11-29 17:05:26 +0100

    wrapper classes

---
+++ src/Day1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Day1 extends DayWrapper {
+
+	@Override
+	Object run(Scanner input) {
+		return input.nextLine();
+	}
+
+	@Override
+	int getDay() {
+		return 1;
+	}
+
+	public static void main(String[] args) {
+		Day1 day1 = new Day1();
+		day1.main();
+	}
+}
