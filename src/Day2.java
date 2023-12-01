d3a8a9e5b93568e026b9f609f57b4348276bd486
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2023-12-01 07:35:44 +0100

    Day 2 init

---
+++ src/Day2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Day2 extends DayWrapper {
+
+	@Override
+	Object run(Scanner input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	int getDay() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		Day2 day2 = new Day2();
+		day2.main();
+	}
+
+}
