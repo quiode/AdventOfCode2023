34198b8cff10617f48168a0a339a51ced7776087
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2023-12-03 11:27:19 +0100

    day4 init

---
+++ resources/Day4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Day4 extends DayWrapper {
+
+	@Override
+	Object run(Scanner input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	int getDay() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		Day4 day4 = new Day4();
+		day4.main();
+	}
+
+}
