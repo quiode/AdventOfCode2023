7a15afb978412794100164ceb3068e230ec42607
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2023-12-04 08:32:58 +0100

    day 5 init

---
+++ src/Day5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Day5 extends DayWrapper {
+
+	@Override
+	Object run(Scanner input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	int getDay() {
+		// TODO Auto-generated method stub
+		return 5;
+	}
+
+	public static void main(String[] args) {
+		Day5 day5 = new Day5();
+		day5.main();
+	}
+
+}
